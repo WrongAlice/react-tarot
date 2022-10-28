afcfcfd7854b6609377839a7b849bf1d14cdc6da
nicole pizzano <pizzano.nicole@gmail.com>
2022-10-28 14:33:18 -0400

    checking img

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import back from '../public/img/back.png'
 
 
 
@@ -9,7 +10,7 @@ export default function SingleCard({card}) {
         <div className="card">
         <div className={flipped ? "flipped" : "undefined"}>
        <img className="front" src={card.src} alt="card front" onClick={setFlipped === "true" } />
-       <img className="back" src="img/back.PNG" alt="card back" onClick={setFlipped } />
+       <img className="back" src="/img/back.png" alt="card back" onClick={setFlipped } />
        <div className="card-info">
         <div className={flipped ? "" : "desc"}>
         
